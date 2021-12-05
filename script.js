b8d5948b85c90d6b6f2211cb5770379254211e2b
Luis Manuel Vivas <luis.manuel.vivas.r@gmail.com>
2021-12-04 23:38:47 -0400

    Calling functions of the constructor from buttons and testing - Luis Manuel Vivas

--- script.js
+++ script.js
@@ -1,11 +1,14 @@
 class Calculator{
     constructor(previousOperandTextElement,currentOperandTextElement){
-        this.previousOperandTextElement = previousOperandTextElement;
-        this.currentOperandTextElement = currentOperandTextElement;
+        this.previousOperandTextElement = previousOperandTextElement
+        this.currentOperandTextElement = currentOperandTextElement
+        this.clear()
     }
 
     clear(){
-
+        this.currentOperand = ''
+        this.previousOperand = ''
+        this.operation = undefined
     }
 
     delete(){
@@ -13,7 +16,7 @@ class Calculator{
     }
 
     appendNumber(number){
-
+        this.currentOperand = number
     }
 
     chooseOperation(operation){
@@ -25,13 +28,24 @@ class Calculator{
     }
 
     updateDisplay(){
-        
+        this.currentOperandTextElement.innerText = this.currentOperand
     }
 }
 
 const numberButtons = document.querySelectorAll('[data-number]')
-const numberButtons = document.querySelectorAll('[data-operation]')
+const OperationButtons = document.querySelectorAll('[data-operation]')
 const equalsButton = document.querySelector('[data-equals]')
 const allClearButton = document.querySelector('[data-all-clear]')
 const previousOperandTextElement = document.querySelector('[data-previous-operand]')
 const currentOperandTextElement = document.querySelector('[data-current-operand]')
+
+const calculator = new Calculator(previousOperandTextElement,currentOperandTextElement)
+
+numberButtons.forEach(button => {
+    button.addEventListener('click', () => {
+
+        calculator.appendNumber(button.innerText)
+        calculator.updateDisplay()
+
+    })
+})
